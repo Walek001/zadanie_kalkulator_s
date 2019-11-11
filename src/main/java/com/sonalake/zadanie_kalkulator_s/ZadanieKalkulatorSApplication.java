3cfccb65572a215541ae8cea8edf6be4e8dde099
jakub.walkowski <jak.walkowski@gmail.com>
2019-11-11 21:07:48 +0100

    init

---
+++ src/main/java/com/sonalake/zadanie_kalkulator_s/ZadanieKalkulatorSApplication.java
@@ -0,0 +1,13 @@
+package com.sonalake.zadanie_kalkulator_s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieKalkulatorSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieKalkulatorSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonalake/zadanie_kalkulator_s/ZadanieKalkulatorSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonalake.zadanie_kalkulator_s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieKalkulatorSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
