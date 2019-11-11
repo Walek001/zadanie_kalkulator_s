e53e6e4ef7f7eb2905f7f19da99e92179c646420
jakub.walkowski <jak.walkowski@gmail.com>
2019-11-12 00:38:53 +0100

    Lombok getters and setter for ExchangeRate

--- src/main/java/com/sonalake/zadanie_kalkulator_s/utils/ExchangeRate.java
+++ src/main/java/com/sonalake/zadanie_kalkulator_s/utils/ExchangeRate.java
@@ -2,9 +2,7 @@ package com.sonalake.zadanie_kalkulator_s.utils;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,10 +11,12 @@ import java.util.Map;
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
 class ExchangeRate {
     @JsonProperty("code") String currencyCode;
-    Float rate;
-    LocalDate lastUpdate;
+    private Float rate;
+    private LocalDate lastUpdate;
     @JsonProperty("rates")
     private void unpackRate(List<Map<String, String>> rates) {
         rate= Float.parseFloat(rates.get(0).get("mid"));
--- src/test/java/com/sonalake/zadanie_kalkulator_s/utils/NBPCurrenciesExchangeImplTest.java
+++ src/test/java/com/sonalake/zadanie_kalkulator_s/utils/NBPCurrenciesExchangeImplTest.java
@@ -15,16 +15,16 @@ class NBPCurrenciesExchangeImplTest {
     @Test
     void getCurrencyExchangeRate_shouldReturnPLNExchangeRate() throws Exception {
         ExchangeRate exchangeRate = nbpCurrenciesExchange.getCurrencyExchangeRate("PLN");
-        assertEquals("PLN", exchangeRate.currencyCode, "Currency code should be PLN");
-        assertEquals(1, exchangeRate.rate,"Exchange rate should be 100");
-        assertEquals(LocalDate.now(), exchangeRate.lastUpdate,"Last date should be today");
+        assertEquals("PLN", exchangeRate.getCurrencyCode(), "Currency code should be PLN");
+        assertEquals(1, exchangeRate.getRate(),"Exchange rate should be 100");
+        assertEquals(LocalDate.now(), exchangeRate.getLastUpdate(),"Last date should be today");
     }
 
     @Test
     void getCurrencyExchangeRate_shouldReturnOtherExchangeRate() throws Exception {
         ExchangeRate exchangeRate = nbpCurrenciesExchange.getCurrencyExchangeRate("USD");
-        assertEquals("USD", exchangeRate.currencyCode, "Currency code should be USD");
-        assertNotEquals(1f, exchangeRate.rate,"Exchange rate shouldn't be 1");
+        assertEquals("USD", exchangeRate.getCurrencyCode(), "Currency code should be USD");
+        assertNotEquals(1f, exchangeRate.getRate(),"Exchange rate shouldn't be 1");
     }
 
     @Test
