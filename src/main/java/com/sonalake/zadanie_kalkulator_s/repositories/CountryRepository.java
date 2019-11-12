83a0f6998ab502c7255dfde204ee40d7261dcb08
jakub.walkowski <jak.walkowski@gmail.com>
2019-11-12 08:42:00 +0100

    Added repositories

---
+++ src/main/java/com/sonalake/zadanie_kalkulator_s/repositories/CountryRepository.java
@@ -0,0 +1,7 @@
+package com.sonalake.zadanie_kalkulator_s.repositories;
+
+import com.sonalake.zadanie_kalkulator_s.models.Country;
+import org.springframework.data.repository.Repository;
+
+public interface CountryRepository extends Repository<Country, Integer> {
+}
---
+++ src/main/java/com/sonalake/zadanie_kalkulator_s/repositories/OfferRepository.java
@@ -0,0 +1,7 @@
+package com.sonalake.zadanie_kalkulator_s.repositories;
+
+import com.sonalake.zadanie_kalkulator_s.models.Offer;
+import org.springframework.data.repository.Repository;
+
+public interface OfferRepository extends Repository<Offer, Integer> {
+}
